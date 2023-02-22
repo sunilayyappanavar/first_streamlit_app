ab16f3e7778eb6db7f74cf495210cdd3472ba2cb
sunilayyappanavar <126055771+sunilayyappanavar@users.noreply.github.com>
2023-02-22 17:37:50 +0530

    corrected typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
 
-steamlit.text("Omega 3 & Bluberry Oatmeal")
-steamlit.text("Kale, Spinach & Rocket Smoothie")
-steamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.text("Omega 3 & Bluberry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
