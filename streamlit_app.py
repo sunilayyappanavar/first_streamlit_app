a1c2f88a20053cf4a6cdfcd5a156f6bf72f791b0
sunilayyappanavar <126055771+sunilayyappanavar@users.noreply.github.com>
2023-02-23 10:30:24 +0530

    using pandas to import csv file and using data

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,4 +13,5 @@ streamlit.text("🥑🍞 Avokado Toast")
 streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🍊🍇")
 
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.multiselect("Pick a fruit", my_fruit_list.index)
 streamlit.dataframe(my_fruit_list)
