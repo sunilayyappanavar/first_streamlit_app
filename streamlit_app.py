d8defc5dfec1483670c8ad92638d92a3686eb186
sunilayyappanavar <126055771+sunilayyappanavar@users.noreply.github.com>
2023-02-23 10:16:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 
 import streamlit
+import pandas
 
 streamlit.title("My Parents New Healthy Diner")
 
@@ -10,3 +11,6 @@ streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avokado Toast")
 
 streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🍊🍇")
+
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
