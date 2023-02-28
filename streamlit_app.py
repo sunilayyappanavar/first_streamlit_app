ce928573f188b628de5e4c5056d8f01041f7a003
sunilayyappanavar <126055771+sunilayyappanavar@users.noreply.github.com>
2023-02-28 18:06:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,12 +18,20 @@ fruits_selected=streamlit.multiselect("Pick some fruits", list(my_fruit_list.ind
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-
+#------------------------------------------------------------
 import requests
 
 streamlit.header("Fruityvice Fruit Advice!")
+
+#asking input from the user
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+#seperate the base url from the fruit name
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+#instead of Kiwi, adding user entered fruit choice, so that user entered fruit is directly fecthed using the api
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 #normalizing json vesion
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
