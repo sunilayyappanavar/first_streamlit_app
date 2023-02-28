ec3cb72806cba41453798ab4a56893216da8119a
sunilayyappanavar <126055771+sunilayyappanavar@users.noreply.github.com>
2023-02-28 17:49:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,4 @@ import requests
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
